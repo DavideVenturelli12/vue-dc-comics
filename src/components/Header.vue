4505651fe0313dd6d590453c94431be9157f370e
Davide Venturelli <dventurelli90@gmail.com>
2022-04-06 15:38:16 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,23 @@
 <template>
-  <header></header>
+  <header>
+    <div>
+      <img src="../assets/dc-logo.png" alt="" />
+    </div>
+    <div>
+      <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIEDOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,5 +26,21 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+header {
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  img {
+    width: 80px;
+    margin: 15px 0;
+  }
+  li {
+    display: inline-block;
+    margin: 10px;
+    font-size: 14px;
+    font-weight: 600;
+  }
+}
 </style>
